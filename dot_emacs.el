c4a674bd55813d174d0519835ea9ad07c77cecf0
lalawue <suchaaa@gmail.com>
2013-11-06 14:42:38 +0800

    update dot emacs

--- dot_emacs.el
+++ dot_emacs.el
@@ -1,5 +1,5 @@
 ;; Sucha's emacs settings
-;; Time-stamp: <13/10/30 22:58>
+;; Time-stamp: <13/11/06 14:41>
 
 ;;{{{ Global Settings
 
@@ -904,6 +904,9 @@
                                      (shrink-window-horizontally -1)))
 
 (global-set-key (kbd "C-,") 'delete-other-windows)
+(Global-set-key (kbd "C-x-w") 
+                '(lambda () (interactive)
+                   delete-window))
 
 ;;}}}
 ;;{{{ My Temp attempt
